1d434ca9fe8a890d759f5a44f4a0b42041066217
Ryan Arqueza <arquezaryan@gmail.com>
2020-12-08 04:41:21 +0800

    Add middleware file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const middlewares = require('./middleware')
+
 const app = express();
 
 
@@ -18,20 +20,8 @@ app.get('/', (req, res) => {
     });
 });
 
-app.use((req, res, next) => {
-    const error = new Error(`Not found — ${req.originalUrl}`);
-    res.status(404);
-    next(error);
-})
-
-app.use((error, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-    res.status(statusCode);
-    res.json({
-        message: error.message,
-        stack: process.env.NODE_ENV === 'production' ? '🍻' : error.stack,
-    })
-});
+app.use(middlewares.notFound)
+app.use(middlewares.errorHandler);
 
 
 const port = process.env.PORT || 1337;
