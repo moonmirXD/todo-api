fd9963ac8a19b3599b3c4f513a8d71d722ef5a9d
Ryan Arqueza <arquezaryan@gmail.com>
2020-12-09 01:36:30 +0800

    Add post route for todo

--- src/api/todos.js
+++ src/api/todos.js
@@ -1,11 +1,23 @@
 const { Router } = require('express');
 
+const Todo = require('../models/Todo');
+
 const router = Router();
 
 router.get('/', (req, res) => {
     res.json({
         message: "🌎"
     })
+})
+
+router.post('/', async (req, res, next) => {
+    try {
+        const todo = new Todo(req.body);
+        const createdTodo = await todo.save();
+        res.json(createdTodo);
+    } catch (error) {
+        next(error);
+    }
 })
 
 module.exports = router
